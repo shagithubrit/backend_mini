ff1df32d7c25bccf3057a1db1503924ea484f43e
shashwat <shashwat.8032@gmail.com>
2024-10-02 12:26:44 +0530

    admin middleware updated

--- middleware/user.js
+++ middleware/user.js
@@ -3,17 +3,30 @@ const { JWT_USER_PASSWORD } = require("../config");
 
 function userMiddleware(req, res, next) {
     const token = req.headers.token;
-    const decoded = jwt.verify(token, JWT_USER_PASSWORD);
-
-    if (decoded) {
-        req.userId = decoded.id;
-        next()
-    } else {
+    try{
+        const decoded = jwt.verify(token, JWT_USER_PASSWORD);
+        if(decoded){
+            req.userId= decode.id;
+            next();
+        }
+        else{
+            res.status(403).json({
+                message: "You are not signe in",
+                error :" you are not signed in",
+                status :403,
+                success : false,
+                data : null
+            })
+        }
+    }catch(error){
         res.status(403).json({
-            message: "You are not signed in"
+            message :  "You  are not signed in",
+            error : " yoh are not signed in",
+            status : 403,
+            success: false,
+            data: null,
         })
     }
-
 }
 
 module.exports = {
